e3b80aa69a66c5b5ea39a1d5199ad9d9e8ca2151
Harley Alexander <harley@mayte.io>
2019-08-17 11:56:05 +1000

    publish cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import dts from "rollup-plugin-dts";
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
