42b7d2ea25ee2321a8098a12722ae209da9300fc
Harley Alexander <harley@mayte.io>
2020-01-21 12:53:36 +1100

    v3.0.4

--- src/useEvent.ts
+++ src/useEvent.ts
@@ -1,35 +1,36 @@
-import { useEffect } from "react";
-import invariant from "invariant";
-import { Channel, PresenceChannel } from "pusher-js";
+import { useEffect } from 'react';
+import invariant from 'invariant';
+import { Channel, PresenceChannel } from 'pusher-js';
 
 /**
- *
+ * Subscribes to a channel event and registers a callback.
  * @param channel Pusher channel to bind to
  * @param eventName Name of event to bind to
  * @param callback Callback to call on a new event
  * @param dependencies Dependencies the callback uses.
  */
-export function useEvent<T>(
-  channel: Channel | PresenceChannel<any> | undefined,
+export function useEvent<D>(
+  channel: Channel | PresenceChannel<unknown> | undefined,
   eventName: string,
-  callback: (data?: T) => void,
-  dependencies: any[] | undefined
+  callback: (data?: D) => void,
+  dependencies?: unknown[] | undefined
 ) {
-  invariant(eventName, "Must supply eventName and callback to onEvent");
-  invariant(callback, "Must supply callback to onEvent");
+  // error when required arguments aren't passed.
+  invariant(eventName, 'Must supply eventName and callback to onEvent');
+  invariant(callback, 'Must supply callback to onEvent');
 
+  // deprecate dependencies
   useEffect(() => {
     if (dependencies) {
       console.warn(
-        "useEvent dependencies are no longer honoured - memoizing the callback is up to the developer."
+        'The useEvent callback is no longer memoized - dependencies are deprecated and its up to you to memoize the callback if you want to.'
       );
     }
   }, [dependencies]);
 
-  // const callbackRef = useCallback<EventCallback>(callback, dependencies);
+  // bind and unbind events whenever the channel, eventName or callback changes.
   useEffect(() => {
     if (channel === undefined) {
-      // console.warn("No channel supplied to onEvent. Not binding callback.");
       return;
     }
     channel.bind(eventName, callback);
