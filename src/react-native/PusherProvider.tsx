85cff6143e6f96e339e5d6ebcd9c8a80b8c8531e
Harley Alexander <harley.alexander.au@gmail.com>
2020-08-19 08:33:52 +1000

    create native index.d.ts

--- src/react-native/PusherProvider.tsx
+++ src/react-native/PusherProvider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Pusher from "pusher-js/react-native";
+import PusherReactNative from "pusher-js/react-native";
 import { PusherProvider as CorePusherProvider } from "../PusherProvider";
 import { PusherProviderProps } from "../types";
 
@@ -7,5 +7,5 @@ export { __PusherContext } from "../PusherProvider";
 
 /** Wrapper around the core PusherProvider that passes in the Pusher react-native lib */
 export const PusherProvider: React.FC<PusherProviderProps> = (props) => (
-  <CorePusherProvider _PusherRuntime={Pusher} {...props} />
+  <CorePusherProvider _PusherRuntime={PusherReactNative} {...props} />
 );
